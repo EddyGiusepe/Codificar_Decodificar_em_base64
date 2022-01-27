e6ae273e9bec8414f7a6f34487136bafb5549d42
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-01-27 20:42:45 -0300

    program de base64

---
+++ base64.py
@@ -0,0 +1,20 @@
+import sys
+import pybase64 as b64
+
+arquivo_leitura = open("texto.txt", "r")
+arquivo_escritura = open("resultado.txt", "w")
+texto_sem_codificar = arquivo_leitura.read()
+
+
+texto_codificado = b64.b64encode(bytes(texto_sem_codificar, "UTF-8"))
+print("O texto codificado é: {}".format(texto_codificado))
+
+
+texto_decodificado = b64.b64decode(texto_codificado)
+print("O texto decodificado é: {}".format(texto_decodificado))
+
+
+arquivo_escritura.write(str(texto_codificado))
+arquivo_escritura.write(str(texto_decodificado))
+arquivo_escritura.close()
+arquivo_leitura.close()
